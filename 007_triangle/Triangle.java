7eed1ae353f388cbf018b0ed707c04ffcb29a46a
Olga Ignatenko <olka@Olgas-MacBook-Pro.local>
2020-12-06 22:49:09 +0100

    Add Triangle solution

--- 007_triangle/Triangle.java
+++ 007_triangle/Triangle.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 
 class Triangle {
     public static void main(String[] args) {
+        double[] sides = new double[3];
         System.out.println("Enter the first side length");
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble();
+        sides[0] = scanner.nextDouble();
         System.out.println("Enter the second side length");
-        double b = scanner.nextDouble();
+        sides[1] = scanner.nextDouble();
         System.out.println("Enter the third side length");
-        double c = scanner.nextDouble();
+        sides[2] = scanner.nextDouble();
+
+        boolean isTriangle = true;
+        for (int i = 0; i < 3; i++) {
+             isTriangle = isTriangle && ((sides[i % 3] + sides[(i + 1) % 3]) > sides[(i + 2) % 3]);
+        }
+
+        if (isTriangle) {System.out.println("It's a triangle");}
+        else System.out.println("It is impossible to build a triangle with this sides length");
     }
 }
