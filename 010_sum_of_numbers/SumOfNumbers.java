1e2701394481355d0459b27bd2ee9a8aa5bb222c
Olga <olha.ignatenko@gmail.com>
2020-12-07 16:15:17 +0100

    Add Sum of Numbers solution

--- 010_sum_of_numbers/SumOfNumbers.java
+++ 010_sum_of_numbers/SumOfNumbers.java
@@ -2,6 +2,7 @@ class SumOfNumbers {
     public static void main(String[] args) {
         Integer sum = 0;
         for (String arg : args) sum += Integer.parseInt(arg);
-        System.out.println (sum);
+        System.out.println("Sum is " + sum);
+        System.out.println("Average value is " + sum / args.length);
     }
 }
